749ec410d49c4c0e1e2625ce666f796bb3724d6f
Ali Ahmad <aliahmad.a@protonmail.com>
2025-05-31 09:04:26 +0500

    Pulumi yaml fixed

--- shared/config/accounts.ts
+++ shared/config/accounts.ts
@@ -6,21 +6,21 @@
 
 const accountsConfig = {
   dev: [
-    { name: "dev-main", email: "dev-main@example.com" }
+    { name: "dev-main", email: "gofoke2293@jeanssi.com" }
   ],
-  sandbox1: [
-    { name: "sandbox1-user1", email: "sandbox1-user1@example.com" },
-    { name: "sandbox1-user2", email: "sandbox1-user2@example.com" }
-  ],
-  sandbox2: [
-    { name: "sandbox2-main", email: "sandbox2-main@example.com" }
-  ],
-  qa: [
-    { name: "qa-main", email: "qa-main@example.com" }
-  ],
-  prod: [
-    { name: "prod-main", email: "prod-main@example.com" }
-  ]
+  // sandbox1: [
+  //   { name: "sandbox1-user1", email: "sandbox1-user1@example.com" },
+  //   { name: "sandbox1-user2", email: "sandbox1-user2@example.com" }
+  // ],
+  // sandbox2: [
+  //   { name: "sandbox2-main", email: "sandbox2-main@example.com" }
+  // ],
+  // qa: [
+  //   { name: "qa-main", email: "qa-main@example.com" }
+  // ],
+  // prod: [
+  //   { name: "prod-main", email: "prod-main@example.com" }
+  // ]
 };
 
 export default accountsConfig; 
